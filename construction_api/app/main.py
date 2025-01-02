64e68b0d6558693c869148ce19ed0231cd45c304
Glaive1232 <arabyan.samvel@student.rau.am>
2025-01-03 01:24:17 +0400

    Added docker-compose

--- construction_api/app/main.py
+++ construction_api/app/main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
 from routers import contractors, objects, customers
 
-app = FastAPI()
+if __name__ == "__main__":
+  app = FastAPI()
 
-# Подключение роутеров
-app.include_router(contractors.router, prefix="/contractors", tags=["Contractors"])
-app.include_router(objects.router, prefix="/objects", tags=["Construction Objects"])
-app.include_router(customers.router, prefix="/customers", tags=["Customers"])
+  # Подключение роутеров
+  app.include_router(contractors.router, prefix="/contractors", tags=["Contractors"])
+  app.include_router(objects.router, prefix="/objects", tags=["Construction Objects"])
+  app.include_router(customers.router, prefix="/customers", tags=["Customers"])
